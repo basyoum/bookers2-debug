3dfba4300c0771df4a0b790a683a39c134322817
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-05-21 01:30:03 +0000

    [Update]1~5、bundle install/rails db:migrate/rails webpacker:install/rails webpacker:compile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      flash[:notice] = "You have updated user successfully."
+      redirect_to users_path(@user)
     else
-      render "show"
+      render :show
     end
   end
 
@@ -34,4 +37,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+
 end
