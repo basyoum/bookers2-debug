9f8c6188ec43471ed6cda25d7dffeee21b0325e9
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-05-22 09:39:17 +0000

    [Update]コメント機能修正2

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,11 +1,6 @@
 class BookComment < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :user
-  belongs_to :book
-=======
   belongs_to :book
   belongs_to :user
 
   validates :comment, presence: true
->>>>>>> topic9
 end
