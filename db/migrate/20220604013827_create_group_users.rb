81dadc0ed572f01f495803c6f7df4ef5ba1b581f
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-06-04 05:46:31 +0000

    [Update]応用課題7c.修正.グループ機能作成.画像表示

--- db/migrate/20220604013827_create_group_users.rb
+++ db/migrate/20220604013827_create_group_users.rb
@@ -1,8 +1,11 @@
 class CreateGroupUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :group_users do |t|
-      t.references :user, foreign_key: true
-      t.references :group, foreign_key: true
+      #t.references :user, foreign_key: true
+      #t.references :group, foreign_key: true
+
+      t.integer :user_id
+      t.integer :group_id
 
       t.timestamps
     end
